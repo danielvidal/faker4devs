8b3aa0aaaf0716a11fc89354b5e406202ffb7f61
Daniel Vidal Salvi Marson <daniel.vidal@youse.com.br>
2020-03-14 15:16:49 -0300

    Add AdSense

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,19 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-160681876-1',
+        trackingId: `UA-160681876-1`,
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
         respectDNT: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-adsense`,
+      options: {
+        publisherId: `ca-pub-8453984344455238`
       },
     },
     `gatsby-plugin-cname`,
